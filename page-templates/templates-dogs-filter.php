d9443adef15aa1d08cc60c12421642e7b3c1d7a7
mifist <avanturka1992@gmail.com>
2017-03-27 03:10:24 +0300

    Вывод вильтра ждя собак (в работе)

--- page-templates/templates-dogs-filter.php
+++ page-templates/templates-dogs-filter.php
@@ -5,26 +5,37 @@ Template Name: Фильтр для собак
 get_header(); ?>
 	
 	<div id="page" role="main">
-		<article class="main-content">
-			<?php query_posts('post_type=bh_dogs&post_status=publish&posts_per_page=9&paged='. get_query_var('paged'))
-			; ?>
+		
+		<div class="page-content">
 			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?><!-- BEGIN of Post -->
+					<?php if ( has_post_thumbnail()) : ?>
+						<?php the_post_thumbnail(); ?>
+					<?php endif; ?>
+					<h3><?php the_title(); ?></h3>
+					<?php the_content(); ?>
+				<?php endwhile; ?><!-- END of Post -->
+			<?php endif; ?>
+		</div>
+		
+		<?php get_template_part( 'template-parts/dogs_filter'); ?>
+		
+	
+		
+		<article class="main-content">
+			
+			<?php query_posts('post_type=dog_list&order=ASC&post_status=publish&posts_per_page=9&paged='.get_query_var('paged')); ?>
+			
+			<?php if ( have_posts() ) : ?> <!-- Start the Loop -->
+				<!-- BEGIN of Post -->
 				
-				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					
 					<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
-						
-						<header>
-							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-							<?php foundationpress_entry_meta(); ?>
-						</header>
-						<div class="entry-content">
-							<?php the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
-						</div>
-						<footer>
-							<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-						</footer>
+						<?php if ( has_post_thumbnail()) : ?>
+							<?php the_post_thumbnail(); ?>
+						<?php endif; ?>
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 						
 					</div>
 				
@@ -32,21 +43,19 @@ get_header(); ?>
 			
 			<?php else : ?>
 				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-			<?php endif; // End have_posts() check. ?>
+			<?php endif; ?> <!-- End the Loop -->
 			
-			<?php /* Display navigation to next/previous pages when applicable */
-			if ( function_exists( 'foundationpress_pagination' ) ) :
+			<!-- Display navigation to next/previous pages when applicable -->
+			<?php if ( function_exists( 'foundationpress_pagination' ) ) :
 				foundationpress_pagination();
-			elseif ( is_paged() ) :
-				?>
+			elseif ( is_paged() ) : ?>
 				<nav id="post-nav">
 					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
 					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
 				</nav>
-			<?php endif; ?>
+			<?php endif; wp_reset_query(); ?>
 		
 		</article>
-		<?php get_sidebar(); ?>
 	
 	</div>
 
